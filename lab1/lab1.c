0d44aa2124105009dc2672c56e1e2adb590f9667
Nicko <nickorussisch@NickoUb>
2016-02-24 14:00:57 +0600

    The first stable version

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "NSort.h"
-
-int getrand(int min, int max)
-{
-    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
-}
+#include "Service.h"
 
 int i, size;
 
@@ -17,20 +13,34 @@ int main(int argc, char *argv[])
         printf("There's no arguments!\n");
         return EXIT_FAILURE;
     }
-    size = atoi (argv[1])-1;
+    size = atoi (argv[1]);
     int32_t *array = (int32_t*) malloc(size * sizeof(int32_t));
     if (array == NULL)
     {
         printf("Error of memory allocation!\n");
         return EXIT_FAILURE;
     }
-    for (i = 0; i < size; i++)
+
+    for (int i = 0; i < size; i++)
     {
-        array[i] = getrand(1, 100);
-        printf("%d ", array[i]);
+        array[i] = getrand(1, 100000);
     }
-    printf("\n\n");
+    
+    double t1 = wtime();
     ISort(array, size);
+    t1 = wtime() - t1;
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = getrand(1, 100000);
+    }
+
+    double t2 = wtime();
+    QuickSort(array, 0, size - 1);
+    t2 = wtime() - t2;
+
+    printf("Executable time InsertionSort: %f seconds.\n", t1);
+    printf("Executable time QuickSort: %f seconds.\n", t2);
     free(array);
     return EXIT_SUCCESS;
 }
