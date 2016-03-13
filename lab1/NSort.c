b8dfa376314542498a15e28a5ab6a21212394c95
nick-ugolnikov <nickorussisch@gmail.com>
2016-03-14 02:40:29 +0600

    Stable version of Counting Sort + fixed important bugs

--- lab1/NSort.c
+++ lab1/NSort.c
@@ -7,35 +7,59 @@
 
 void CountingSort(uint32_t *array, int size) // Counting Sort
 {
-    uint32_t *count = (uint32_t *)malloc(size * sizeof(uint32_t));
+    int i, min = array[0], max = array[0];
+
+    for (i = 0; i < size; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    int csize = max - min + 1;
+
+    uint32_t *count = (uint32_t *)malloc((csize + 1) * sizeof(uint32_t));
     uint32_t *arraycopy = (uint32_t *)malloc(size * sizeof(uint32_t));
-  
+
     if (count == NULL || arraycopy == NULL)
     {
         printf("Error of memory allocation!\n");
         return EXIT_FAILURE;
     }
   
-    for (int i = 0; i < size; i++)
+    for (i = 0; i < csize + 1; i++)
     {
         count[i] = 0;
     }
   
-    for (int i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
-        count[array[i]] += 1;
+        count[array[i] - min + 1] += 1;
     }
-  
-    int x = 0;
-    for (int i = 0; i < size; i++)
+
+    for (i = 0; i < csize; i++)
     {
-        for (int j = 0; j < count[i]; j++)
-        {
-            arraycopy[x] = i;
-            x++;
-        }
+        count[i] += count[i - 1];
     }
+
+    for (i = size - 1; i >= 0; i--)
+    {
+        arraycopy[count[array[i] - min]] = array[i];
+        count[array[i] - min]++;
+    }
+
     free(count);
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = arraycopy[i];
+    }
+    
     free(arraycopy);
 }
 
